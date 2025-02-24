d5464d094a16a2fe14ad7d0851ad980f23549eb1
Ayushcode04 <ayushsinghg04@gmail.com>
2025-02-25 02:22:37 +0530

    fixed ui of Home Page

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -3,10 +3,12 @@ import Sidebar from "../../components/sidebar/sidebar"
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className="flex items-center justify-center min-h-screen">
+    <div className='flex h-[500px]  md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
 			 <Sidebar/>
 			<MessageContainer />
 		</div>
+    </div>
   )
 }
 export default Home;
